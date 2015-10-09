51330c6c03751d614fba4d0ce85eef64b0b1f148
Joseph Lejtman <lejtmany@gmail.com>
2015-10-08 21:50:23 -0400

    added tests. Gulp now runs tests automatically.

--- Mail/tests.py
+++ Mail/tests.py
@@ -1,42 +1,96 @@
 from django.test import TestCase
 import Mail.views
-from Mail.models import Email
+from Mail.models import Email, Contact
 from django.core.urlresolvers import reverse
 from django.utils import timezone
+from django.template import defaultfilters
+# from selenium.webdriver.chrome.webdriver import WebDriver
+# from django.test import LiveServerTestCase
 
-def create_email(sender, receiver, date = timezone.now(), subject = 'Empty', content = 'Empty', is_deleted = False, is_read = False):
+
+def create_email(sender='sender@gmail.com', receiver='receiver@gmail.com', date=timezone.now(), subject='Empty',
+                 content='Empty', is_deleted=False, is_read=False):
     return Email.objects.create(sender=sender, receiver=receiver, date=date, subject=subject,
                                 content=content, is_deleted=is_deleted, is_read=is_read)
 
-def assert_queryset_objects_equal(self,context_queryset, other_queryset, ordered=False):
+def create_contact(first_name='John', last_name='Doe', dob=timezone.now(), email='jdoe@gmail.com'):
+    return Contact.objects.create(first_name=first_name, last_name=last_name, dob=dob, email=email)
+
+
+def assert_queryset_objects_equal(self, context_queryset, other_queryset, ordered=False):
     return self.assertQuerysetEqual(context_queryset, map(repr, other_queryset), ordered=ordered)
 
 
 # Create your tests here.
 class InboxTests(TestCase):
-
     def test_inbox_doesnt_contain_mail_sent_by_owner(self):
         create_email(sender=Mail.views.my_email, receiver='otherguy@gmail.com')
-        email_not_from_self = create_email(sender='someone@gmail.com', receiver='otherguy@gmail.com')
+        email_not_from_self = create_email()
         response = self.client.get(reverse('mail:inbox'))
         self.assertEqual(response.status_code, 200)
-        assert_queryset_objects_equal(self, response.context['emails'], [email_not_from_self,])
+        assert_queryset_objects_equal(self, response.context['emails'], [email_not_from_self, ])
 
     def test_inbox_doesnt_contain_deleted_mail(self):
-        create_email(sender='someguy@gmail.com', receiver='otherguy@gmail.com', is_deleted=True)
-        email_not_deleted = create_email(sender='someone@gmail.com', receiver='otherguy@gmail.com', is_deleted=False)
+        create_email(is_deleted=True)
+        email_not_deleted = create_email(is_deleted=False)
         response = self.client.get(reverse('mail:inbox'))
         self.assertEqual(response.status_code, 200)
-        assert_queryset_objects_equal(self, response.context['emails'], [email_not_deleted,])
+        assert_queryset_objects_equal(self, response.context['emails'], [email_not_deleted, ])
 
     def test_inbox_with_search_params(self):
-        create_email(sender='someguy@gmail.com', receiver='otherguy@gmail.com')
-        target_sender = create_email(sender='tArget@gmail.com', receiver='otherguy@gmail.com')
-        target_content = create_email(sender='someguy@gmail.com', receiver='otherguy@gmail.com', content='TArGEThsbdfkjdnfksd')
-        target_subject = create_email(sender='someguy@gmail.com', receiver='somereceiver@gmail.com' ,subject='subjectTargETsubject')
+        create_email()
+        target_sender = create_email(sender='tArget@gmail.com')
+        target_content = create_email(content='TArGEThsbdfkjdnfksd')
+        target_subject = create_email(subject='subjectTargETsubject')
         response = self.client.get(reverse('mail:inbox') + '?searchParams=TaRGet')
         self.assertEqual(response.status_code, 200)
-        assert_queryset_objects_equal(self, response.context['emails'], [target_sender, target_content, target_subject,])
+        assert_queryset_objects_equal(self, response.context['emails'],
+                                      [target_sender, target_content, target_subject, ])
 
 
-# class EmailDetailTests(TestCase):
+class EmailDetailTests(TestCase):
+
+    def test_email_detail_populates(self):
+        now = timezone.now()
+        sender = 'sender@gmail.com'
+        receiver = 'receiver@gmail.com'
+        content = 'hello world'
+        create_email(sender=sender, receiver=receiver, date=now, content=content)
+        response = self.client.get(reverse('mail:email_detail', args=[1]))
+        self.assertContains(response, sender,
+                            status_code=200)
+        self.assertContains(response, content,
+                            status_code=200)
+        self.assertContains(response, defaultfilters.date(now),
+                            status_code=200)
+
+class ComposeEmailTests(TestCase):
+
+    def test_compose_email_populates_receiver(self):
+        receiver = 'receiver@gmail.com'
+        create_email(receiver=receiver)
+        response = self.client.get(reverse('mail:compose_email') + '?compose_to=' + receiver)
+        self.assertContains(response, receiver,
+                            status_code=200)
+
+class SentEmailListTests(TestCase):
+
+    def test_sent_emails_list_only_contains_sent_emails(self):
+        bad_message = 'Stranger!'
+        create_email(sender=Mail.views.my_email)
+        create_email(sender=Mail.views.my_email)
+        create_email(sender='other_person@gmail.com', content=bad_message)
+        response = self.client.get(reverse('mail:sent_emails'))
+        self.assertNotContains(response, bad_message,
+                            status_code=200)
+
+class ContactListTests(TestCase):
+
+    def test_search_for_contact(self):
+        fname_match = create_contact(first_name='MArv')
+        lname_match =create_contact(last_name='MaRv')
+        email_match =create_contact(email='MarV@gmail.com')
+        no_match = create_contact()
+        response = self.client.get(reverse('mail:contacts')+ '?searchParams=MARV')
+        self.assertEqual(response.status_code, 200)
+        assert_queryset_objects_equal(self, response.context['contacts'], [fname_match, lname_match,email_match])
--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var less = require('gulp-less');
-var debug = require('gulp-debug');
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
-var gutil = require('gulp-util');
-var merge = require('merge-stream')
+var shell = require('gulp-shell')
 
 gulp.task('move_vars', function(){
 	return gulp.src('./Mail/static/Mail/css/variables.less')
@@ -30,13 +26,12 @@ gulp.task('less', ['move_vars'],function(){
 gulp.task('watch', function(){
 	gulp.watch('./Mail/static/Mail/css/variables.less', ['less']);
   	gulp.watch('less/*.less', ['less']);
+  	gulp.watch(['./Mail/**/*.html', './Mail/**/*.py'], ['run-tests']);
 });
 
-gulp.task('default',['less','watch']);
+gulp.task('run-tests', shell.task([
+'python3 manage.py test']))
 
-gulp.task('webserver', function(){
-	connect.server(
-    {livereload: true}
-    );
-});
+gulp.task('default',['less','run-tests','watch']);
+
 
